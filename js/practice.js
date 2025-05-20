a0349490525910b4d8a2850d277807bd3872093e
marharyta zozulia <zozulia.marharyta@gmail.com>
2025-05-20 20:24:41 +0200

    js

---
+++ js/practice.js
@@ -0,0 +1,107 @@
+
+
+// const courses = ["HTML", "CSS" , " React" ];
+
+// const lastIndex = courses.length - 1;
+
+// courses[2] = "Node.js";
+ 
+// console.log(courses.length);
+
+
+
+
+
+
+// let a = [1,2,3];
+// let b = a; 
+// a[0] = 100;
+// b++
+
+// console.log("a", a);
+// console.log("b", b);
+
+
+
+
+
+
+
+
+
+// const coarses = ["HTML", "CSS", " React"];
+
+// const str = courses.join(" ");
+// const arr = str.split();
+
+// console.log(str);
+// console.log(arr);
+// const arr = courses.slice(1, 3);
+// console.log(courses);
+// console.log(arr);
+
+// const arr = [1, 2, 3];
+
+// const result = courses.concat(arr, ["lalala", "tototo"], courses),;
+// console.log(result);
+
+
+
+// const index = courses.indexOf("React");
+// console.log(index);
+
+
+
+// courses.push("Node.js");
+
+
+
+
+// const a = ourses.pop();
+// console.log("a",a)
+// console.log(courses);
+
+
+
+
+
+// const friends = ["Rechal", "Monica", "Phoebe", "Joey"];
+
+// for (let i = 0; i < friends.length; i++) {
+//     friends[i] = friends[i] + "!!!";
+//     //  or  friends[i] += "111";
+    
+// }
+// console.table(friends);
+
+
+
+
+
+
+// for (const item of friends) {
+//     console.log(item);
+// }
+
+
+// console.log(friends.includes("Joey"));
+
+/// plosha
+// const value = "8 10";
+// const arr = value.split(" ");
+// const res = arr[0] * arr[1];
+
+
+
+
+// /// perimetr
+// const res = (Number(arr[0]) + Number(arr[1])) * 2;
+
+
+// console.log(res);
+
+
+// const fruits = ["🍎", "🍇", "🍑", "🍌", "🍋"];
+// for (let i = 0; i < fruits.length; i++){
+//     console.log(`${i + 1}: ${fruits[i]}`);
+// }
